336846c625bf1334e172bbfc70e57eb03a14c7cd
roan <rsxkeys@protonmail.com>
2021-11-11 09:30:24 +0000

    Add 2 buttons to the bottom menu to switch between graphs and raw data

--- project/src/gui/MenuOptions.java
+++ project/src/gui/MenuOptions.java
@@ -8,5 +8,7 @@ public class MenuOptions extends JPanel {
         FlowLayout layout = new FlowLayout(FlowLayout.CENTER,10,10);
         setLayout( layout );
         add( new JButton("EXPORT TO PDF"), BorderLayout.SOUTH );
+        add( new JButton("GRAPH"), BorderLayout.SOUTH );
+        add( new JButton("RAW DATA"), BorderLayout.SOUTH );
     }
 }
