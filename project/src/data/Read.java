bfb6a04d7b70776ccde20c2a9d5e230a1626c9e8
mh20198 <mh20198@essex.ac.uk>
2021-11-11 22:28:50 +0000

    unzip gzipped csv data format

--- project/src/data/Read.java
+++ project/src/data/Read.java
@@ -1,20 +1,25 @@
 package data;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.InputStream;
-import java.net.URL;
+import java.io.*;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.CopyOption;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
+import java.net.http.HttpClient;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipException;
 
 class Read {
 
     public static List<String> dates = new ArrayList<>();
     public static List<String> cases = new ArrayList<>();
 
-    public static void CsvReader(String filename) throws Exception {
+    public static void CsvReader(String filename) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = br.readLine()) != null) {
@@ -25,20 +30,34 @@ class Read {
         }
     }
 
-    public static void downloadFile(URL url, String fileName) throws Exception {
-        try (InputStream in = url.openStream()) {
-            Files.copy(in, Paths.get(fileName));
-        }
+    public static void downloadFile(String uri, String filename) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(uri)).build();
+        HttpResponse<InputStream> response = client.send(request, HttpResponse.BodyHandlers.ofInputStream());
+        if (response.statusCode() != 200) throw new IOException();
+        var gzip = new GZIPInputStream(response.body());
+        Files.copy(gzip, Path.of(filename), StandardCopyOption.REPLACE_EXISTING);
     }
 
-    public static void main(String[] args) throws Exception {
-        //URL url = new URL("https://coronavirus.data.gov.uk/api/v1/data?filters=areaType=overview&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newDeaths28DaysByDeathDate%22:%22newDeaths28DaysByDeathDate%22,%22cumDeaths28DaysByDeathDate%22:%22cumDeaths28DaysByDeathDate%22%7D&format=csv");
-        //URL url2 = new URL("https://coronavirus.data.gov.uk/api/v1/data?filters=areaType=overview&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22%7D&format=csv");
-        //downloadFile(url, "data_2021-Nov-03.csv");
-        //(new File("data_2021-Nov-03.csv")).renameTo(new File("deaths.csv"));
-        //downloadFile(url2, "data_2021-Nov-03.csv");
-        //(new File("data_2021-Nov-03.csv")).renameTo(new File("cases.csv"));
-        CsvReader("cases.csv");
+    public static void main(String[] args) {
+        String url = "https://coronavirus.data.gov.uk/api/v1/data?filters=areaType=overview&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newDeaths28DaysByDeathDate%22:%22newDeaths28DaysByDeathDate%22,%22cumDeaths28DaysByDeathDate%22:%22cumDeaths28DaysByDeathDate%22%7D&format=csv";
+        String url2 = "https://coronavirus.data.gov.uk/api/v1/data?filters=areaType=overview&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22%7D&format=csv";
+        try {
+            downloadFile(url, "resources/deaths.csv");
+            downloadFile(url2, "resources/cases.csv");
+        } catch(ZipException e) {
+            System.out.println("Failed to extract CSV data due to invalid gzip stream.");
+        } catch(IOException e) {
+            System.out.println("Failed to get CSV data.");
+        } catch(InterruptedException e) {
+            System.out.println("CSV data download interrupted.");
+        }
+        try {
+            CsvReader("resources/deaths.csv");
+            CsvReader("resources/cases.csv");
+        } catch(IOException e) {
+            System.out.println("Failed to read CSV data.");
+        }
         for (int i = 0; i < cases.size(); i++){
             System.out.println(dates.get(i));
             System.out.println(cases.get(i));
